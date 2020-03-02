c9f8986ed25d59115265d1e6ad4ea9bf7f01497a
JuanDuran85 <duran85juan@gmail.com>
2020-03-02 11:38:05 -0300

    agregando ejercicio 3

---
+++ ejercicio_3/src/index.js
@@ -0,0 +1,6 @@
+const num1 = 23;
+let nombre = "Alumnos";
+let num2 = 34;
+let suma = num1+num2;
+
+console.log(`Saludos desde archivo index en JS ${num1} ${nombre} . El resultadod de la suma es ${suma}`);
