e85ceca3f0c8acdc2e592739882e6dc4098d0578
JuanDuran85 <duran85juan@gmail.com>
2020-03-04 10:24:22 -0300

    modificando ejercicio 6

--- ejercicio_6/routes/users.js
+++ ejercicio_6/routes/users.js
@@ -5,6 +5,29 @@ var router = express.Router();
 /* Post user  */ 
 router.post("/", function(req, res, next) {
   userController.user_create(req, res, next);
+  let nombre = req.body.nombre;
+  let apellido = req.body.apellido;
+  let correo = req.body.correo;
+  let clave = req.body.clave;
+  res.send(`
+    <table>
+      <tbody>
+        <tr>
+          <td>Nombre</td>
+          <td>Apellido</td>
+          <td>Correo</td>
+          <td>Clave</td>
+        </tr>
+        <tr>
+          <td>${nombre}</td>
+          <td>${apellido}</td>
+          <td>${correo}</td>
+          <td>${clave}</td>
+        </tr>
+      </tbody>
+    </table>  
+  `);
+
 });
 
 module.exports = router;
